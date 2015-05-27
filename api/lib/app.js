e5657311a76abeebe1ccb5693887ef7516db6290
Daniel Fang <danfang@uw.edu>
2015-05-27 22:41:51 +0000

    File structure

--- api/lib/app.js
+++ api/lib/app.js
@@ -1,24 +1,17 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var Me = require('./routes/me');
 
 var app = express();
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Pretty print JSON
 app.set('json spaces', 4);
 
+// Allow Ajax GET calls
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
