e25ad62786d7d812208cddabde3b036351dbfc57
Daniel Fang <danfang@uw.edu>
2015-05-27 10:37:21 +0000

    Updating client code

--- client/public/javascripts/ui.js
+++ client/public/javascripts/ui.js
@@ -9,22 +9,19 @@ var RouteHandler = Router.RouteHandler;
 var App = React.createClass({
   render: function () {
     return (
-      <div>
+      <div className="container">
         <header id="nav">
           <ul>
             <li><Link to="app">Home</Link></li>
             <li><Link to="blog">Blog</Link></li>
             <li><Link to="code">Code</Link></li>
-            <li><Link to="tweets">Tweets</Link></li>
-            <li><Link to="checkins">Checkins</Link></li>
-            <li><Link to="photos">Photos</Link></li>
           </ul>
         </header>
 
         <RouteHandler/>
 
         <footer>
-        	<p>Made with Me API</p>
+        	<p>Made with Me API &amp; React &amp; Sass</p>
         </footer>
       </div>
     );
@@ -33,26 +30,81 @@ var App = React.createClass({
 
 var Home = React.createClass({
 	getInitialState: function() {
-		return { me: null };
+		return { me: null, tweets: [], checkins: [] };
 	},
 	componentDidMount: function() {
 		$.get(API_URL, function(data) {
 			console.log(data);
 			this.setState({ me: data.me });
 		}.bind(this));
+
+		$.get(API_URL + "twitter", function(data) {
+			console.log(data);
+			this.setState({ tweets: data.tweets });
+		}.bind(this));
+
+		$.get(API_URL + "location", function(data) {
+			console.log(data);
+			this.setState({ checkins: data.checkins.items });
+		}.bind(this));
 	},
 	render: function() { 
 		var me = this.state.me;
 		if (!me) return <div></div>;
+		var checkinNode = this.state.checkins.length ? <Checkin checkin={this.state.checkins[0]} /> : "";
+		var tweetNode = this.state.tweets.length ? <Tweet tweet={this.state.tweets[0]} /> : "";
 		return (
 			<div id="home">
-				<h1>Home</h1>
-				<h2>{me.name}</h2>
-				<p>{me.bio}</p>
+				<div className="overview">
+					<h2>{me.name}</h2>
+					<p>{me.bio}</p>
+				</div>
+				<div id="latest-checkin">
+					{checkinNode}
+				</div>
+				<div id="latest-tweet">
+					{tweetNode}
+				</div>
 			</div>
 		);
 	}}
 );
+
+var Checkin = React.createClass({
+	render: function() {
+		var checkin = this.props.checkin;
+		return (
+			<div className="checkin media">
+				<div className="media-left">
+					<i className="fa fa-2x fa-map-marker"></i>
+				</div>
+				<div className="media-body">
+					<p className="location">{checkin.venue.name}</p>
+					<p className="shout">{checkin.shout}</p>
+					<p className="date">{moment(checkin.createdAt * 1000).fromNow()}</p>
+				</div>
+			</div>
+		);
+	}
+})
+
+var Tweet = React.createClass({
+	render: function() {
+		var tweet = this.props.tweet;
+		return (
+			<div className="tweet media">
+				<div className="media-left">
+					<i className="fa fa-2x fa-twitter"></i>
+				</div>
+				<div className="media-body">
+					<p className="screenName">@{tweet.user.screen_name}</p>
+					<p className="text">{tweet.text}</p>
+					<p className="date">{moment(tweet.created_at).fromNow()}</p>
+				</div>
+			</div>
+		);
+	}
+})
 
 var Blog = React.createClass({
 	getInitialState: function() {
@@ -95,87 +147,68 @@ var Code = React.createClass({
 	render: function() { 
 		if (!this.state.events.length) return <div></div>;
 		var eventNodes = this.state.events.map(function(event) {
-			return (
-				<div>
-					<h2>{event.type}</h2>
-					<a href={"https://github.com/"+ event.repo.name}><p>{event.repo.name}</p></a>
-					<p>{(new Date(event.created_at)).toDateString()}</p>
-				</div>
-			);
+			if (event.type == 'WatchEvent') return <WatchEvent event={event} />;
+			if (event.type == 'CreateEvent') return <CreateEvent event={event} />;
+			if (event.type == 'PushEvent') return <PushEvent event={event} />;
 		})
-		return <div><h1>Code</h1>{eventNodes}</div>
+		return <div id="code">{eventNodes}</div>
 	}}
 );
 
-var Tweets = React.createClass({
-	getInitialState: function() {
-		return { tweets: [] };
-	},
-	componentDidMount: function() {
-		$.get(API_URL + "twitter", function(data) {
-			console.log(data);
-			this.setState({ tweets: data.tweets });
-		}.bind(this));
-	},
-	render: function() { 
-		if (!this.state.tweets.length) return <div></div>;
-		var tweetNodes = this.state.tweets.map(function(tweet) {
-			return (
-				<div>
-					<h2>{tweet.text}</h2>
-					<p>Created at {(new Date(tweet.created_at)).toDateString()}</p>
-				</div>
-			);
-		});
-		return <div><h1>Tweets</h1>{tweetNodes}</div>
-	}}
-);
+var WatchEvent = React.createClass({
+	render: function() {
+		var event = this.props.event;
+		var repo = event.repo;
+		return (
+			<div className="github-event">
+				<p className="title">
+					<i className="fa fa-star fa-lg"></i>
+					Starred <a href={"https://github.com/" + repo.name}>{repo.name}</a>
+				</p>
+				<p className="date">{moment(event.created_at).fromNow()}</p>
+			</div>
+		);
+	}
+});
 
-var Checkins = React.createClass({
-	getInitialState: function() {
-		return { checkins: [] };
-	},
-	componentDidMount: function() {
-		$.get(API_URL + "location", function(data) {
-			console.log(data);
-			this.setState({ checkins: data.checkins.items });
-		}.bind(this));
-	},
-	render: function() { 		
-		if (!this.state.checkins.length) return <div></div>;
-		var checkinNodes = this.state.checkins.map(function(checkin) {
-			return (
-				<div>
-					<h2>{checkin.shout}</h2>
-					<p>{checkin.type}</p>
-					<p>{checkin.venue.name}</p>
-					<p>{checkin.venue.location.city} {checkin.venue.location.state}</p>
-					<p>{(new Date(checkin.createdAt * 1000)).toDateString()}</p>
-				</div>
-			);
-		});
-		return <div><h1>Checkins</h1>{checkinNodes}</div>
-	}}
-);
+var CreateEvent = React.createClass({
+	render: function() {
+		var event = this.props.event;
+		var repo = event.repo;
+		return (
+			<div className="github-event">
+				<p className="title">
+					<i className="fa fa-code-fork fa-lg"></i>
+					Created {event.payload.ref_type} {event.payload.ref} in
+					<a href={"https://github.com/" + repo.name}> {repo.name}</a>
+				</p>
+				<p className="date">{moment(event.created_at).fromNow()}</p>
+			</div>
+		);
+	}
+});
 
-var Photos = React.createClass({
-	componentDidMount: function() {
-		$.get(API_URL + "photos", function(data) {
-			console.log(data);
-		});
-	},
-	render: function() { 
-		return <div>Photos</div>
-	}}
-);
-
+var PushEvent = React.createClass({
+	render: function() {
+		var event = this.props.event;
+		var repo = event.repo;
+		return (
+			<div className="github-event">
+				<p className="title">
+					<i className="fa fa-code fa-lg"></i>
+					Pushed {event.payload.commits.length} commit{event.payload.commits.length == 1 ? "" : "s"} to
+					<a href={"https://github.com/" + repo.name}> {repo.name}</a>
+				</p>
+				<p className="commit">Branch {event.payload.ref} at {event.payload.head}</p>
+				<p className="date">{moment(event.created_at).fromNow()}</p>
+			</div>
+		);
+	}
+});
 var routes = (
   <Route name="app" path="/" handler={App}>
     <Route name="blog" handler={Blog}/>
     <Route name="code" handler={Code}/>
-    <Route name="tweets" handler={Tweets}/>
-    <Route name="checkins" handler={Checkins}/>
-    <Route name="photos" handler={Photos}/>
     <DefaultRoute handler={Home}/>
   </Route>
 );
