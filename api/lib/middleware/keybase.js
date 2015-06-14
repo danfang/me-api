7739e7ba5f1ec63cec2500d7b1e1075d2ffc42fc
magdev <magdev3.0@gmail.com>
2015-06-14 08:50:01 +0200

    added route to download the raw public-key

--- api/lib/middleware/keybase.js
+++ api/lib/middleware/keybase.js
@@ -23,6 +23,24 @@ var Keybase = {
                     return res.json(data.them);
                 });
             }
+        },
+        {
+            method: "GET",
+            path: "/key.asc",
+            handler: function(req, res) {
+                var cachedResult = cache.get('keybase');
+                if (cachedResult) {
+                    return res.send(cachedResult);
+                }
+                
+                var url = 'https://keybase.io/' + this.me + '/key.asc';
+                request({ url: url, headers: { 'User-Agent': this.me }}, function(err, response, body) {
+                    if (err || response.statusCode != 200) return handleError(err, res);
+                    cache.put('keybase', body, DEFAULT_CACHE_MSEC);
+                    console.log('cache miss');
+                    return res.send(body);
+                });
+            }
         }
     ],
 }
