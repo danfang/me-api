c936f68482c154e9b2834c5bd105405cff85ee5f
Daniel Fang <danfang@uw.edu>
2015-08-29 16:46:23 -0700

    Fix style

--- api/Gruntfile.js
+++ api/Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jscs')
+  grunt.loadNpmTasks('grunt-jscs');
   grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('default', ['jshint', 'mochaTest']);
