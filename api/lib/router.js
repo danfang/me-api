ea66f24301689c13c8768586d0bb539e97ed0dbd
Daniel Fang <danfang@uw.edu>
2015-08-29 20:29:53 -0700

    Finishing router tests

--- api/lib/router.js
+++ api/lib/router.js
@@ -2,26 +2,43 @@ var express = require('express');
 
 // This is the extensibility portion of Me API, where you can bind a list of
 // modules from config.json and me.json to any given endpoint.
-module.exports = function(me, modules, settings) {
-  this.router = express.Router();
-  this.modules = modules;
-  this.settings = settings;
-  this.routes = [];
+module.exports = (function() {
+  function ApiRouter(me, modules, settings) {
+    this.router = express.Router();
+    this.modules = modules;
+    this.settings = settings;
+    this.routes = [];
 
-  // Function that reads data from modules.json, mounts it on a specified path,
-  // and binds given data to that request.
-  this.useModule = function(path, module) {
-    var data = this.modules[module.source].data;
-    data.host = this.settings.host;
+    this.router.get('/', function(req, res) {
+      res.json({ me: me, routes: this.routes });
+    }.bind(this));
 
-    // Preprocesses data if there is a given pre() function in the module.
-    if (module.pre) module.pre(data);
+    this.initialize(modules);
+  }
 
-    this.routes.push(path);
-    this.mountModuleRoutes(path, module.routes, data);
+  ApiRouter.prototype.initialize = function(modules) {
+    for (var moduleName in modules) {
+      var config = modules[moduleName]; // configuration object for each module
+      this.useModule(moduleName, config);
+    }
   };
 
-  this.mountModuleRoutes = function(path, moduleRoutes, data) {
+  ApiRouter.prototype.useModule = function(name, config) {
+    var data = config.data;
+    var path = config.path;
+    var module = config.custom ? require(name) : require('./middleware/' + name);
+    data.host = this.settings.host; // used for various hooks and callback URIs
+
+    if (module.pre) {
+      module.pre(data); // Preprocesses data if given a pre() function
+    }
+
+    this.mountModuleRoutes(path, module.routes, data);
+    this.routes.push(path);
+    console.log('Using module: ' + name + ' on ' + path);
+  };
+
+  ApiRouter.prototype.mountModuleRoutes = function(path, moduleRoutes, data) {
     moduleRoutes.forEach(function(route) {
       var endpoint = path + route.path;
       if (route.method == 'GET') {
@@ -32,22 +49,5 @@ module.exports = function(me, modules, settings) {
     }, this);
   };
 
-  this.initialize = function(modules) {
-    for (var moduleName in modules) {
-      var settings = modules[moduleName];
-      if (settings.custom) {
-        this.useModule(settings.path, require(moduleName));
-        console.log('Using custom module: ' + moduleName + ' on ' + settings.path);
-      } else {
-        this.useModule(settings.path, require('./middleware/' + moduleName));
-        console.log('Using standard module: ' + moduleName + ' on ' + settings.path);
-      }
-    }
-  };
-
-  this.router.get('/', function(req, res) {
-    res.json({ me: me, routes: this.routes });
-  }.bind(this));
-
-  this.initialize(modules);
-};
+  return ApiRouter;
+})();
